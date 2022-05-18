a837bc7534661560c89829a95fcf145ae98ae94f
Win Feigle <win.feigle@gmail.com>
2022-05-18 17:06:38 -0600

    commit tree df089ee0dc29226c4735a0695d318d3c4a686d65

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr){
+  return arr.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(arr){
+    return arr.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = int => {
+  const newFare = fare => fare * int;
+  return newFare;
+}
+
+const fareDoubler = fare => {
+  return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = fare => {
+  return createFareMultiplier(3)(fare);
+}
+
+const selectDifferentDrivers = (drivers, currentGroup) => {
+  return currentGroup(drivers);
+}
